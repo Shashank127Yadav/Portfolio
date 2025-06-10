7c26ae5d733b11c03da0ffcaf4ee5b400c258a3d
Shashank Yadav <125982193+Shashank127Yadav@users.noreply.github.com>
2025-06-10 19:26:31 +0530

    updated resume link

--- src/data/constants.js
+++ src/data/constants.js
@@ -12,8 +12,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges . With a passion for learning . I iam dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Shashank127Yadav ",
   resume:
-    "https://drive.google.com/file/d/103JAhJbVIZnfRgiMo3-N9wy8y47Dx9pi/view?usp=sharing",
-  linkedin: "https://www.linkedin.com/in/shashank-yadav-3b4187269/",
+    "https://drive.google.com/file/d/1ffEUsOWvvsP1p5MQ-0nBVGXMPt7Gwqau/view?usp=sharing",
   // twitter: "https://twitter.com/RishavChanda",
   // insta: "https://www.instagram.com/monikayadav7/",
   // facebook: "https://www.facebook.com/rishav.chanda.165/",
